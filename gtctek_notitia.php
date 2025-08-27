354288131e78ed6b55c3f9754a64adc56ec7c409
Nick Watt <nick@gtctek.co.uk>
2025-08-27 16:07:53 +0100

    Plugin base created

---
+++ gtctek_notitia.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package GtctekNotitia
+ * @version 0.0.1
+ * 
+ * Plugin Name:  Notitia
+ * Text Domain:  gtctek-notitia
+ * Plugin URI:   https://notitia.co.uk
+ * Description:  Notitia is a WordPress plugin that provides comprehensive site management features, including user roles, event scheduling, and content organization.
+ * Version:      0.0.1
+ * Author:       Gtctek
+ * Author URI:   https://gtctek.co.uk
+ * License:      GPL2 or later
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+$gtctek_notitia_ver = '0.0.1';
+
+if (!defined('ABSPATH')) {
+    die( 'Invalid request.' );
+    exit;
+}
+
+// Define global properties
+define( 'GTCTEK_NOTITIA__VERSION', $gtctek_notitia_ver );
+define( 'GTCTEK_NOTITIA__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'GTCTEK_NOTITIA__PLUGIN_URI', plugin_dir_url( __FILE__ ) );
+define( 'GTCTEK_NOTITIA__PREPEND', 'gtctek' );
+define( 'GTCTEK_NOTITIA__TEXT_DOMAIN', 'gtctek-notitia' );
