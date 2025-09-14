cf9a62761f0ee635463c6c344e39d2b1d4bd2981
Nick Watt <nick@gtctek.co.uk>
2025-09-14 21:47:21 +0100

    Viewport meta added to admin section

--- classes/class.gtctek.notitia.core.php
+++ classes/class.gtctek.notitia.core.php
@@ -9,7 +9,15 @@
         }
 
         private static function init_hooks() {
+            if ( is_admin() ) {
+                add_action( 'wp_head', [ self::class, 'add_meta_viewport' ] );
+            }
+
             self::$initiated = true;
+        }
+
+        public static function add_meta_viewport() {
+            echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
         }
     }
 ?>
