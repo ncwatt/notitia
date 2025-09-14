7e8e2d8496350290641616210be246fc81ec03a5
Nick Watt <nick@gtctek.co.uk>
2025-09-14 21:50:28 +0100

    Bootstrap added to admin section

--- classes/class.gtctek.notitia.core.php
+++ classes/class.gtctek.notitia.core.php
@@ -11,6 +11,8 @@
         private static function init_hooks() {
             if ( is_admin() ) {
                 add_action( 'wp_head', [ self::class, 'add_meta_viewport' ] );
+                add_action( 'admin_enqueue_scripts', [ self::class, 'load_stylesheets' ] );
+                add_action( 'admin_enqueue_scripts', [ self::class, 'load_javascript' ] );
             }
 
             self::$initiated = true;
@@ -18,6 +20,14 @@
 
         public static function add_meta_viewport() {
             echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+        }
+
+        public static function load_stylesheets() {
+            wp_enqueue_style( 'bootstrap', GTCTEK_NOTITIA__PLUGIN_URI . 'assets/css/bootstrap.min.css', '', '5.3.3', 'all' );
+        }
+
+        public static function load_javascript() {
+            wp_enqueue_script( 'bootstrap', GTCTEK_NOTITIA__PLUGIN_URI . 'assets/js/bootstrap.min.js', '', '5.3.3', 'all' );
         }
     }
 ?>
